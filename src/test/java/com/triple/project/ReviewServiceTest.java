035a18552d63e45056718f2d31457e2c7161661d
pu1et <pu1etproof@gmail.com>
2022-07-11 09:27:20 +0900

    test(review) : 리뷰 수정 테스트 작성

--- src/test/java/com/triple/project/ReviewServiceTest.java
+++ src/test/java/com/triple/project/ReviewServiceTest.java
@@ -9,6 +9,8 @@ import com.triple.project.dto.ReviewDTO;
 import com.triple.project.service.MemberService;
 import com.triple.project.service.PlaceService;
 import com.triple.project.service.ReviewService;
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,23 +34,62 @@ public class ReviewServiceTest {
 	@DisplayName("리뷰를 작성한다.")
 	@Test
 	void createReview() {
-		final String reviewId = "240a0658-dc5f-4878-9381-ebb7b2667772";
-		final String content = "좋아요!";
-		final List<String> attachedPhotoIds = new ArrayList<>();
-		attachedPhotoIds.add("e4d1a64e-a531-46de-88d0-ff0ed70c0bb8");
-		attachedPhotoIds.add("afb0cef2-851d-4a50-bb07-9cc15cbdc332");
-		final String userId = "3ede0ef2-92b7-4817-a5f3-0c575361f745";
-		final String placeId = "2e4baf1c-5acb-4efb-a1af-eddada31b00f";
-
-		Member member = memberService.saveMember(new MemberDTO("3ede0ef2-92b7-4817-a5f3-0c575361f745"));
-		Place place = placeService.savePlace(new PlaceDTO("2e4baf1c-5acb-4efb-a1af-eddada31b00f"));
-		ReviewDTO reviewDTO = ReviewDTO.builder()
-				.reviewId(reviewId)
-				.content(content)
-				.attachedPhotoIds(attachedPhotoIds)
-				.memberId(userId)
-				.placeId(placeId).build();
-		Review review = reviewService.createReview(reviewDTO);
+		TestReviewDTO testReviewDTO = new TestReviewDTO();
+		Member member = memberService.saveMember(new MemberDTO(testReviewDTO.memberId));
+		Place place = placeService.savePlace(new PlaceDTO(testReviewDTO.placeId));
+		ReviewDTO.CreateRequest reviewDTO = testReviewDTO.getCreateReviewDTO();
+		Review review = reviewService.createReview(testReviewDTO.getCreateReviewDTO());
 		Assertions.assertTrue(reviewDTO.isEqualReview(review));
+	}
+
+	@DisplayName("리뷰를 수정한다.")
+	@Test
+	void updateReview() {
+		TestReviewDTO testReviewDTO = new TestReviewDTO();
+		Member member = memberService.saveMember(new MemberDTO(testReviewDTO.memberId));
+		Place place = placeService.savePlace(new PlaceDTO(testReviewDTO.placeId));
+		Review review = reviewService.createReview(testReviewDTO.getCreateReviewDTO());
+		final String updateContent = "싫어요!";
+		final List<String> updateAttachedPhotoIds = new ArrayList<>();
+		updateAttachedPhotoIds.add("e4d1a64e-a531-46de-88d0-ff0ed70c0bb8");
+		testReviewDTO.setContent(updateContent);
+		testReviewDTO.setAttachedPhotoIds(updateAttachedPhotoIds);
+		ReviewDTO.UpdateRequest reviewDTO = new ReviewDTO.UpdateRequest(updateContent, updateAttachedPhotoIds);
+
+		Review updateReview = reviewService.updateReview(testReviewDTO.reviewId, reviewDTO);
+
+		Assertions.assertTrue(reviewDTO.isEqualReview(updateReview));
+	}
+
+	@Getter
+	static class TestReviewDTO {
+		private final String reviewId = "240a0658-dc5f-4878-9381-ebb7b2667772";
+		private String content = "좋아요!";
+		private List<String> attachedPhotoIds;
+		private final String memberId = "3ede0ef2-92b7-4817-a5f3-0c575361f745";
+		private final String placeId = "2e4baf1c-5acb-4efb-a1af-eddada31b00f";
+
+		public TestReviewDTO() {
+			attachedPhotoIds = new ArrayList<>();
+			attachedPhotoIds.add("e4d1a64e-a531-46de-88d0-ff0ed70c0bb8");
+			attachedPhotoIds.add("afb0cef2-851d-4a50-bb07-9cc15cbdc332");
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
+
+		public void setAttachedPhotoIds(List<String> attachedPhotoIds) {
+			this.attachedPhotoIds = attachedPhotoIds;
+		}
+
+		public ReviewDTO.CreateRequest getCreateReviewDTO() {
+			return ReviewDTO.CreateRequest.builder()
+					.reviewId(reviewId)
+					.content(content)
+					.attachedPhotoIds(attachedPhotoIds)
+					.memberId(memberId)
+					.placeId(placeId).build();
+		}
 	}
 }
